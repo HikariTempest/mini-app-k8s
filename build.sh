fd9f34ff06d0f55a83b1af5ec0f60d0d509d0c1e
HikariTempest <arwed@gmx.at>
2024-03-05 12:08:16 +0100

    github actions update

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+repo="${GITHUB_REPOSITORY,,}"
+
 mvn -B package
 cp src/main/docker/Dockerfile target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
-docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
-docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
+docker build --tag ghcr.io/$repo/backend:latest ./target
+docker push ghcr.io/$repo/backend:latest
